691508381d32fda6fd1876c8869446a11ee73ecc
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-12 13:35:49 +0100

    printed list

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,31 @@ const genres = []; // ?? Questo è l'array che userò nel select
 
 for (let i = 0; i < films.length; i++) {
   const currentGenre = films[i].genre;
-  console.log(currentGenre);
-  if (genres.includes(currentGenre)) {
-    break;
-  } else {
-    {
-      genres.push(currentGenre);
-    }
+  if (!genres.includes(currentGenre)) {
+    genres.push(currentGenre);
+    console.table(genres);
   }
-  console.table(genres);
 }
 
 export default function App() {
   return (
     <>
-      <section>
-        <div className="container text-center">
-          <h1>CIAOOOO</h1>
+      <section className="mt-5">
+        <div className="container text-center p-5">
+          <h1 className="text-start mb-4">Catalogo film:</h1>
+          <ul className="list-group list-group-flush text-start">
+            {films.map((film, index) => {
+              return (
+                <li
+                  key={index}
+                  className="list-group-item d-flex justify-content-between"
+                >
+                  {film.title}
+                  <p className="d-inline">{film.genre}</p>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </section>
     </>
