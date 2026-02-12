2a1ebe1563925a4d4ad0ed3c44da03b6a24e1e97
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-12 13:00:04 +0100

    git init

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+const films = [
+  { title: "Inception", genre: "Fantascienza" },
+  { title: "Il Padrino", genre: "Thriller" },
+  { title: "Titanic", genre: "Romantico" },
+  { title: "Batman", genre: "Azione" },
+  { title: "Interstellar", genre: "Fantascienza" },
+  { title: "Pulp Fiction", genre: "Thriller" },
+];
+
 export default function App() {
   return (
     <>
